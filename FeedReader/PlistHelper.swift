0382c83295afc6d8cec87f2b706340bb73e4fada
Ron Daryl Magno <ron.zohan@gmail.com>
2018-02-06 02:37:47 +0800

    Added plist helper

---
+++ FeedReader/PlistHelper.swift
@@ -0,0 +1,41 @@
+//
+//  PlistHelper.swift
+//  FeedReader
+//
+//  Created by Ron Daryl Magno on 2/6/18.
+//  Copyright © 2018 Ron Daryl Magno. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+public enum PlistKey {
+    case urls
+
+    func value() -> String {
+        switch self {
+        case .urls:
+            return "urls"
+        }
+    }
+}
+public struct Environment {
+    
+    fileprivate var infoDict: [String: Any]  {
+        get {
+            if let dict = Bundle.main.infoDictionary {
+                return dict
+            }else {
+                fatalError("Plist file not found")
+            }
+        }
+    }
+
+    public func configuration<T>(_ key: PlistKey) -> T {
+        switch key {
+        case .urls:
+            return infoDict[PlistKey.urls.value()] as! T
+        }
+    }
+}
